d585548db48802f5bebbf58f22744c9fc91d0673
nicolamarino <nicolamarino527@gmail.com>
2025-02-24 20:45:03 +0100

    [feat] add pages route

--- src/App.jsx
+++ src/App.jsx
@@ -4,5 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 // importiamo gli elementi della libreria di gestione delle rotte
-import { BrowserRouter, Routers, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Homepage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import Contacts from './pages/Contacts'
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/contacts' element={<Contacts />} />
+        </Routes>
+      </BrowserRouter >
+    </>
+  )
+}
+
+export default App
