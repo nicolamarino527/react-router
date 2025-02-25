7296e99f271268501306dc76dcc4bed57c256892
nicolamarino <nicolamarino527@gmail.com>
2025-02-25 17:21:12 +0100

    [feat] add axios call on post detail

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,7 +1,29 @@
+import axios from "axios"
+import { useParams, } from "react-router-dom"
+import { useState, useEffect, use } from "react"
+
+
 export default function PostDetail() {
+
+    // desctucturing per riornare l'id
+    const { id } = useParams()
+
+    // settiamo lo stato al componente
+    const [post, setPost] = useState({})
+
+    // chamiamo la rotta store
+    function fetchPost() {
+        axios.get(`http://localhost:3000/posts/${id} `)
+            .then(response => setPost(response.data))
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => fetchPost(),
+        [])
+
     return (
         <>
-            <h1>dettaglio</h1>
+            <h1>{post.title}</h1>
         </>
     )
 }
--- src/pages/PostDetailPage.jsx
+++ src/pages/PostDetailPage.jsx
@@ -3,8 +3,12 @@ import PostDetail from "../components/PostDetail"
 export default function PostDetailPage() {
     return (
         <>
-            <h1>pagina di dettaglio</h1>
-            <PostDetail />
+            <div className="bg">
+                <div className="container">
+                    <h1>pagina di dettaglio</h1>
+                    <PostDetail />
+                </div>
+            </div>
         </>
     )
 }
